1c2691cc48fcd7febf59483d36622218d6057dd8
QuantumGlitch <soaxpi@gmail.com>
2020-10-06 01:05:19 +0200

    fixed peer dependencies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require('../mongoose') || require('mongoose');
+const mongoose = require.main.require('mongoose');
 const RefConstraintError = require('./error');
 
 const refs = {};
